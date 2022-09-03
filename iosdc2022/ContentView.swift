b337760fac0f05abe9aa784c0636a43de507048c
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2022-09-03 17:26:05 +0900

    :sparkles: Create base project

---
+++ iosdc2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosdc2022
+//
+//  Created by AkkeyLab on 2022/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosdc2022/iosdc2022App.swift
@@ -0,0 +1,17 @@
+//
+//  iosdc2022App.swift
+//  iosdc2022
+//
+//  Created by AkkeyLab on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct iosdc2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
